bebc0b27b13062fe5a218708e82d27e5ed9c8994
João Leodoro <joaooleodoro@gmail.com>
2025-03-07 18:36:31 -0300

    add about and advantages sections

--- script.js
+++ script.js
@@ -13,52 +13,52 @@ document.addEventListener("DOMContentLoaded", function() {
             duration: 1
         })
 
-        gsap.fromTo("#heroTitle", {
-            y: -40,
-            opacity: 0 }, {
-                y: 0,
-                opacity: 1,
-                duration: 0.5
-            })
+    gsap.fromTo("#heroTitle", {
+        y: -40,
+        opacity: 0 }, {
+            y: 0,
+            opacity: 1,
+            duration: 0.5
+        })
 
-            gsap.fromTo("#heroSubtitle", {
-                y: -40,
-                opacity: 0 }, {
-                    y: 0,
-                    opacity: 1,
-                    duration: 0.5,
-                    delay: 0.2
-                })
+    gsap.fromTo("#heroSubtitle", {
+        y: -40,
+        opacity: 0 }, {
+            y: 0,
+            opacity: 1,
+            duration: 0.5,
+            delay: 0.2
+        })
 
-            gsap.fromTo("#heroBtn", {
-                opacity: 0 }, {
-                    opacity: 1,
-                    duration: 0.5,
-                    delay: 0.4,
-                    onComplete: function () {
-                        gsap.fromTo("#widgets-l", {
-                            x: -700,
-                            opacity: 0 }, {
-                                x: 0,
+    gsap.fromTo("#heroBtn", {
+        opacity: 0 }, {
+            opacity: 1,
+            duration: 0.5,
+            delay: 0.4,
+            onComplete: function () {
+                gsap.fromTo("#widgets-l", {
+                    x: -700,
+                    opacity: 0 }, {
+                        x: 0,
+                        opacity: 1,
+                        duration: 1
+                    });
+                
+                gsap.fromTo("#widgets-r", {
+                    x: 700,
+                    opacity: 0 }, {
+                        x: 0,
+                        opacity: 1,
+                        duration: 1,
+                        onComplete: function () {
+                            gsap.to("#discover", {
                                 opacity: 1,
                                 duration: 1
-                            });
-                        
-                        gsap.fromTo("#widgets-r", {
-                            x: 700,
-                            opacity: 0 }, {
-                                x: 0,
-                                opacity: 1,
-                                duration: 1,
-                                onComplete: function () {
-                                    gsap.to("#discover", {
-                                        opacity: 1,
-                                        duration: 1
-                                    })
-                                }
-                            });
-                    }
-                })
+                            })
+                        }
+                    });
+            }
+        })
 
     gsap.fromTo("#mockup1", {
         x: -200,
@@ -95,6 +95,59 @@ document.addEventListener("DOMContentLoaded", function() {
                 trigger: "#mockups",
             }
         })
+
+    gsap.fromTo("#aboutTitle", {
+        y: -40 }, {
+            y: 0,
+            opacity: 1,
+            duration: 0.5,
+            scrollTrigger:{
+                trigger: "#about"
+            }
+        })
+
+    gsap.fromTo("#aboutSubtitle", 
+        { y: -40, opacity: 0 }, 
+        { 
+            y: 0,
+            opacity: 1,
+            duration: 0.5,
+            delay: 0.2,
+            onComplete: function () {
+                gsap.fromTo("#creatorImg, #creatorText, #ideaText", 
+                    { y: -40, opacity: 0 }, 
+                    { y: 0, opacity: 1, duration: 0.5 }
+                );
+
+            },
+            scrollTrigger: { 
+                trigger: "#about",
+                start: "top 80%",
+                toggleActions: "play none none none"
+            }
+        }
+    );
+
+    gsap.fromTo("#adTitle", 
+        { y: -40 }, 
+        { 
+            y: 0,
+            opacity: 1,
+            duration: 0.5,
+            onComplete: function () {
+                gsap.to("#ad1, #ad2, #ad3, #ad4", 
+                    { y: 0, opacity: 1, duration: 0.5 }
+                );
+
+            },
+            scrollTrigger: { 
+                trigger: "#advantages",
+                start: "top 80%",
+                toggleActions: "play none none none"
+            }
+        }
+    );
+        
 });
 
 
